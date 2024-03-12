05b1a89e9a1f8ebe3f9d382aeca227a0d6f53a22
chigivera <ayman.benchamkha@example.com>
2024-03-12 12:41:07 +0100

    second commit

--- cryptage.c
+++ cryptage.c
@@ -15,11 +15,25 @@ void cryptage(char p[],int decalage) {
     }
 }
 
+void decryptage(char p[],int decalage) {
+    int i = 0;
+    while(p[i]!='\0') {
+        p[i] -= decalage;
+        if(p[i] < 31) {
+            p[i] -= 255;
+        } 
+        i++;
+    }
+}
+
 
 int main() {
     char password[20];
     scanf("%s",&password);
+    printf("# Cryptage: \n");
     cryptage(password,1);
+    printf("# Decryptage: \n");
+    decryptage(password,1);
     printf("\n%s",password);
     
 }
